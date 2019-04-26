ee60ef3160128c73c5f66011b2da18508f06e9b9
rhkdals2222 <49803525+rhkdals2222@users.noreply.github.com>
2019-04-26 21:26:05 +0900

    Update main_MLEcomp.R

--- MLEexp/main_MLEcomp.R
+++ MLEexp/main_MLEcomp.R
@@ -1,11 +1,33 @@
 ##comparison
 
-source("MLEexp/simulsetting.R")
-n=30
-p=30
+
+library(rslurm)
+
+
+MLEcomp=function(n,p,k=NULL,method="band",filename=NULL,wd=NULL,dir=NULL){
+  ##input : n,p,k,method
+  ##output : Sigma0, MLE1, MLE2
+  source("MLEexp/simulsetting.R")
+  datawt=simulsetting(n=n,p=p,k=k,method=method)
+  source("MLEexp/MLE_bandconst.R")
+  #res=MLE_bandconst(datawt$X,k=k)
+  if(!is.null(filename)){
+    saveRDS(list(Sigma0=datawt$Sigma0,const_MLE=MLE_bandconst(datawt$X,k=k)$Sigma,MLE=t(datawt$X)%*%datawt$X/n),
+            file = paste(wd,dir,filename,sep = "/"))  
+  }else{
+    return(list(Sigma0=datawt$Sigma0,const_MLE=MLE_bandconst(datawt$X,k=k)$Sigma,MLE=t(datawt$X)%*%datawt$X/n))  
+  }
+}
+
+
+n=10
+p=10
 k=5
-datawt=simulsetting(n=n,p=p,k=k)
-
-
-source("MLEexp/MLE_bandconst.R")
-res=MLE_bandconst(datawt$X,k=k)
+method="band"
+mydir="testMLE"
+#rr=MLEcomp(n,p,k)
+siter=25
+for(i in 1:siter){
+  sjob<- slurm_call(MLEcomp, list(n=n,p=p,k=k,method=method,
+                                  filename=paste0("MLE",i,".rds"),wd=getwd(),dir=mydir))
+}
